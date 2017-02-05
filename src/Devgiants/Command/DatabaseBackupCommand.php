b82a50475caf6d3f8b5aade44f8fa6de3916052f
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-02-05 15:18:19 +0100

    Add YAML component + start to define command for database backup

--- src/Devgiants/Command/DatabaseBackupCommand.php
+++ src/Devgiants/Command/DatabaseBackupCommand.php
@@ -9,27 +9,49 @@ namespace Devgiants\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
 
 class DatabaseBackupCommand extends Command
 {
+    const FILE_OPTION = "file";
+
+    /**
+     * @inheritdoc
+     */
     protected function configure()
     {
         $this
-            // the name of the command (the part after "bin/console")
             ->setName('database')
-
-            // the short description shown while running "php bin/console list"
             ->setDescription('Backup databases accordingly to the parameters provided or YML configuration file')
-
-            // the full command description shown when running the command with
-            // the "--help" option
             ->setHelp("This command allows you to save databases")
+            ->addOption(self::FILE_OPTION, "f", InputOption::VALUE_OPTIONAL, "The YML configuration file")
         ;
     }
 
+    /**
+     * @inheritdoc
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello World');
+        // Get conf file
+        $ymlFile = $input->getOption(self::FILE_OPTION);
+
+        if($ymlFile !== null && is_file($ymlFile)) {
+            try {
+                $configuration = Yaml::parse(file_get_contents($ymlFile));
+                // TODO add structure check
+                // TODO add treatment
+            } catch (ParseException $e) {
+                $output->writeln("<error>Unable to parse the YAML string : {$e->getMessage()}</error>");
+            } catch (\Exception $e) {
+                $output->writeln("<error>Error happened : {$e->getMessage()}</error>");
+            }
+        }
+        else {
+            $output->writeln("<error>Filename is not correct : {$ymlFile}</error>");
+        }
     }
 }
