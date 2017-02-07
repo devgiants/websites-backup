c465fb7387f59ab9bf73ef5f23deff1e5de9e1ba
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-02-07 18:59:59 +0100

    Database export and command line info in progress

--- src/Devgiants/Command/DatabaseBackupCommand.php
+++ src/Devgiants/Command/DatabaseBackupCommand.php
@@ -17,6 +17,13 @@ use Symfony\Component\Yaml\Yaml;
 class DatabaseBackupCommand extends Command
 {
     const FILE_OPTION = "file";
+    const ROOT_TEMP_PATH = "/tmp/website-backups/";
+    const DATABASE = "database";
+    const FILES = 'files';
+    const TEMP_PATHS = [
+        self::DATABASE => self::ROOT_TEMP_PATH . "databases",
+        self::FILES => self::ROOT_TEMP_PATH . "files"
+    ];
 
     /**
      * @inheritdoc
@@ -24,9 +31,9 @@ class DatabaseBackupCommand extends Command
     protected function configure()
     {
         $this
-            ->setName('database')
-            ->setDescription('Backup databases accordingly to the parameters provided or YML configuration file')
-            ->setHelp("This command allows you to save databases")
+            ->setName('save')
+            ->setDescription('Backup sites accordingly to the parameters provided or YML configuration file')
+            ->setHelp("This command allows you to save sites")
             ->addOption(self::FILE_OPTION, "f", InputOption::VALUE_OPTIONAL, "The YML configuration file")
         ;
     }
@@ -43,7 +50,38 @@ class DatabaseBackupCommand extends Command
             try {
                 $configuration = Yaml::parse(file_get_contents($ymlFile));
                 // TODO add structure check
-                // TODO add treatment
+
+                // Create temp path to store dumps+files temporarily
+                $output->write("Create temp folders...");
+                foreach(self::TEMP_PATHS as $tempPath) {
+                    if(!file_exists($tempPath)) {
+                        mkdir($tempPath, 0777, true);
+                    }
+                }
+                $output->write("<info>DONE</info>" . PHP_EOL);
+
+                $output->writeln("Start sites backup");
+                foreach($configuration['sites'] as $site => $siteConfiguration) {
+                    $output->writeln("<comment> - Site {$site}</comment>");
+
+                    $output->write("  - Connection to database server...");
+                    $mysqli = new \mysqli($siteConfiguration['database']['server'], $siteConfiguration['database']['user'], $siteConfiguration['database']['password']);
+
+                    // Handle connection failure
+                    if ($mysqli->connect_errno) {
+                        $output->writeln("<error>  - Connection failure : {$mysqli->connect_error}</error>");
+                        // TODO logs + mails
+                        exit();
+                    }
+                    $output->write("<info>DONE</info>" . PHP_EOL);
+                    $dumpName = "{$site}_" . date('YmdHis') . ".sql.gz";
+
+                    $output->write("  - Start database export and compression...");
+                    exec("mysqldump --user={$siteConfiguration['database']['user']} --password='{$siteConfiguration['database']['password']}' --single-transaction {$siteConfiguration['database']['name']} | gzip > " . self::TEMP_PATHS[self::DATABASE] . "/{$dumpName}");
+                    $output->write("<info>DONE</info>" . PHP_EOL);
+                }
+                $output->writeln("End sites backup");
+
             } catch (ParseException $e) {
                 $output->writeln("<error>Unable to parse the YAML string : {$e->getMessage()}</error>");
             } catch (\Exception $e) {
