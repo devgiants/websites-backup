629f2226f1e3a7c9ef2e2b4703924c786113bf9e
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-04-07 10:55:32 +0200

    Add update command for enabling self update

--- src/Devgiants/Command/UpdateCommand.php
+++ src/Devgiants/Command/UpdateCommand.php
@@ -9,7 +9,33 @@
 namespace Devgiants\Command;
 
 
-class UpdateCommand
-{
+use Herrera\Phar\Update\Manager;
+use Herrera\Phar\Update\Manifest;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+class UpdateCommand extends Command
+{
+    const MANIFEST_FILE = 'http://mattketmo.github.io/cliph/manifest.json';
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('self-update')
+            ->setDescription('Updates website-backup.phar to the latest version')
+        ;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
+        $manager->update($this->getApplication()->getVersion(), true);
+    }
 }
