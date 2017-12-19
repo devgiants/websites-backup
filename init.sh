6956316f41f198323af3700c9cf3659a45220f80
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-12-19 17:16:31 +0100

    Add Docker stack around app

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o allexport
+source ./.env
+set +o allexport
+
+# Create mysql volume target dir if not exists already, and set it as current user (needed for container mapping)
+mkdir -p ${MYSQL_HOST_VOLUME_PATH}
+sudo chown -R ${HOST_USER}:${HOST_USER} ${MYSQL_HOST_VOLUME_PATH}
+sudo chmod -R 775 ${MYSQL_HOST_VOLUME_PATH}
+
+# Create target dir if not exists already, and set it as current user (needed for container mapping)
+mkdir -p ${SYMFONY_HOST_RELATIVE_APP_PATH}
+sudo chown -R ${HOST_USER}:${HOST_USER} ${SYMFONY_HOST_RELATIVE_APP_PATH}
+sudo chmod -R 775 ${SYMFONY_HOST_RELATIVE_APP_PATH}
